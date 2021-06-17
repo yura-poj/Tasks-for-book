3c8758837b20ee5c5eacc51fe5d9ca4f6b3bd400
yura-poj <85958170+yura-poj@users.noreply.github.com>
2021-06-18 00:34:25 +0700

    New Task

--- 1.rb
+++ 1.rb
@@ -1,1 +1,18 @@
-puts 'Hello'
+#code return your fio or profession
+class User
+  def fio(fio_inf)
+    puts fio_inf
+  end
+  def profession(prof)
+    puts prof
+  end
+end
+
+puts 'Whats your fio'
+fio = gets
+puts 'Whats your profession'
+prof = gets
+
+I = User.new
+I.fio(fio)
+I.profession(prof)
